b102211f550eabe6d49a57bf42de0a385cd1c17d
Jun Jiang <smujiang@gmail.com>
2024-04-05 11:17:09 -0500

    Update create_run_bash.py

--- release/create_run_bash.py
+++ release/create_run_bash.py
@@ -9,7 +9,8 @@ core_list = open(core_list_fn, 'r').readlines()
 wrt_str = ""
 for i in core_list:
     i = i.strip()
-    command = "python cli_seg_plus.py -s %s/%s -t %s/%s -o %s -v\n" % (HE_dir, i, MxIF_dir, i, output_dir)
+    # command = "python cli_seg_plus.py -s %s/%s -t %s/%s -o %s -v\n" % (HE_dir, i, MxIF_dir, i, output_dir)
+    command = "python cli_seg_plus.py -s %s/%s -t %s/%s -o %s \n" % (HE_dir, i, MxIF_dir, i, output_dir)
     wrt_str += command
 fn = "./run_sec1.sh"
 fp = open(fn, 'w')
@@ -21,7 +22,8 @@ output_dir = "/infodev1/non-phi-data/junjiang/Ovarian_TMA/test_align_wsi/Sec2"
 wrt_str = ""
 for i in core_list:
     i = i.strip()
-    command = "python cli_seg_plus.py -s %s/%s -t %s/%s -o %s -v\n" % (HE_dir, i, MxIF_dir, i, output_dir)
+    # command = "python cli_seg_plus.py -s %s/%s -t %s/%s -o %s -v\n" % (HE_dir, i, MxIF_dir, i, output_dir)
+    command = "python cli_seg_plus.py -s %s/%s -t %s/%s -o %s \n" % (HE_dir, i, MxIF_dir, i, output_dir)
     wrt_str += command
 fn = "./run_sec2.sh"
 fp = open(fn, 'w')
